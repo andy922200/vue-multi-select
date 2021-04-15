49ded28c8a6d52aa7c7d3344acbd6a592cadde57
andy lien <andy922200@gmail.com>
2021-04-16 02:35:07 +0800

    1. update component Name

--- dev/serve.ts
+++ dev/serve.ts
@@ -4,8 +4,8 @@ import Vue, {
 import Dev from './serve.vue'
 // To register individual components where they are used (serve.vue) instead of using the
 // library as a whole, comment/remove this import and it's corresponding "Vue.use" call
-import VueSelector from '@/entry.esm'
-Vue.use(VueSelector)
+import VueMultiSelect from '@/entry.esm'
+Vue.use(VueMultiSelect)
 
 Vue.config.productionTip = false
 
